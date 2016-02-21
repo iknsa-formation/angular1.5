cced31cffb9f1aac3f5903e88045cd7a3942f771
Khalid Sookia <khalidsookia@gmail.com>
2016-02-21 21:56:23 +0100

    Add todo

--- scripts/app.js
+++ scripts/app.js
@@ -34,4 +34,14 @@ todoApp.controller('TodoListController', function($scope) {
             done : false
         }
     ];
+
+    todoList.addTodo = function() {
+        // On rajoute dans le tableau todoList.todos le nouveau todo avec par défaut la valeur de done à false
+        todoList.todos.push({
+            text: todoList.formNewTodo,
+            done: false
+        });
+        // Ensuite on vide le champ
+        todoList.formNewTodo = '';
+    }
 });
