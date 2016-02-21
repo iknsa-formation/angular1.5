0cc6594685bcf880615814dfd8b887962ad7bfd6
Khalid Sookia <khalidsookia@gmail.com>
2016-02-21 18:56:03 +0100

    step2-solution

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,10 @@
  *  pour notre application todoApp puisque c'est le nom qu'on lui a donné dans notre directive
  */
 var todoApp = angular.module('todoApp', []);
+
+/**
+ * Chainage du controller au module avec le même nom qu'on lui a donné dans notre vue.
+ */
+todoApp.controller('TodoListController', function() {
+    console.log("TodoListController");
+});
