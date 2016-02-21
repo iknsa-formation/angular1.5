70c7a23b3fe6f220a29513bc3f9bf8050edf6950
Khalid Sookia <khalidsookia@gmail.com>
2016-02-21 18:49:23 +0100

    typo

--- scripts/app.js
+++ scripts/app.js
@@ -6,4 +6,4 @@
  * Instatiation du module todoApp. C'est ici que angular cherchera le code a exécuter
  *  pour notre application todoApp puisque c'est le nom qu'on lui a donné dans notre directive
  */
-angular.module('todoApp', []);
+var todoApp = angular.module('todoApp', []);
