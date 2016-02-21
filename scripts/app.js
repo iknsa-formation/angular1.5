01a2b77745510e50144f696ac1022770f28fc657
Khalid Sookia <khalidsookia@gmail.com>
2016-02-21 18:33:54 +0100

    step-1 solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 /**
  * @author Khalid Sookia <khalidsookia@gmail.com>
  */
+
+/**
+ * Instatiation du module todoApp. C'est ici que angular cherchera le code a exécuter
+ *  pour notre application todoApp puisque c'est le nom qu'on lui a donné dans notre directive
+ */
+angular.module('todoApp', []);
