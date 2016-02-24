29998c0a7138b02fed87a84b9cd8e5b11da0e694
Khalid Sookia <khalidsookia@gmail.com>
2016-02-24 22:51:21 +0100

    Override email validation

--- scripts/app.js
+++ scripts/app.js
@@ -47,17 +47,30 @@ app.config(function($routeProvider) {
 });
 
 app.directive('maDirective', function() {
+    return {
+        restrict: 'EA',
+        template: '<p>My new directive</p>'
+    }
+});
 
-  return {
+app.directive('realEmail', function() {
+    var regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-    restrict: 'EA',
+    return {
+        // Si ngModel est présent
+        require: '?ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            // On applique le validateur seulement si angular a ajouter son validateur email
+            if (ctrl && ctrl.$validators.email) {
 
-    template: '<p>My new directive</p>'
-
-  }
-
-
-
+                // Cette partie la surcharge le validateur par défaut d'angular
+                ctrl.$validators.email = function(modelValue) {
+                    // Retourne un booléen
+                    return ctrl.$isEmpty(modelValue) || regex.test(modelValue);
+                };
+            }
+        }
+    };
 });
 
 app.controller('AppController', ['$scope', function($scope){
